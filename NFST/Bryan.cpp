5710d2a1bde032f90e5fc5b5446965836123a822
Fabian68 <fabiantsch@gmail.com>
2023-08-16 17:09:34 +0200

    Nerf du personnage Bryan

--- NFST/Bryan.cpp
+++ NFST/Bryan.cpp
@@ -2,31 +2,31 @@
 #include "Aleatoire.h"
 #include "Affichage.h"
 
-Bryan::Bryan(Experiences E, Orbes O, Animaux A, Objets Obj) : Personnage(5, E, O, A, Obj, "Bryan", 1, 5, 4, 30, 30, -100, 0, 0, 30, 3) {
-	ajouterCoupCritique(10);
+Bryan::Bryan(Experiences E, Orbes O, Animaux A, Objets Obj) : Personnage(5, E, O, A, Obj, "Bryan", 1, 4, 4, 15, 15, -100, 0, 0, 20, 3) {
+	ajouterCoupCritique(5);
 	ajouterDegatsCritique(25);
 }
 
-void Bryan::attaqueEnnemis(sf::RenderWindow* window, std::vector< sf::Sound >& allSounds)
+void Bryan::attaqueEnnemis(Combat & C, sf::RenderWindow* window, std::vector< sf::Sound >& allSounds)
 {
 	int choix = choixAttaque();
 	int DEGATS;
 	int SOINS;
 	int indice;
 	double RATIO;
-	DEGATS = static_cast<int>(degats(0.10, 0.30) * (1.0 + degatsCritiques() / 100.0));
+	DEGATS = static_cast<int>(degats(0.05, 0.25) * (1.0 + degatsCritiques() / 100.0));
 	Affichage().dessinerTexte(nom() + "Attaque critique",window);
-	Attaque(DEGATS, equipeEnnemi().plusFaible(),window,allSounds);
+	Attaque(DEGATS, equipeEnnemi().plusFaible(), C, window, allSounds);
 	switch (choix) {
 
 	case 0:
 		if (equipeEnnemi().estEnVie()) {
 			DEGATS = degats(0.45, 0.75);
 			Affichage().dessinerTexte(nom() + " coup de dague ! ",window);
-			Attaque(DEGATS, equipeEnnemi().plusProcheVivant(),window,allSounds);
+			Attaque(DEGATS, equipeEnnemi().plusProcheVivant(), C, window, allSounds);
 			if (attaqueDouble() && equipeEnnemi().estEnVie()) {
 				DEGATS = degats(0.12, 0.24);
-				AttaqueBrut(DEGATS, equipeEnnemi().plusProcheVivant(),window);
+				AttaqueBrut(DEGATS, equipeEnnemi().plusProcheVivant(),C,window);
 			}
 		}
 		ajouterMana(2);
@@ -34,23 +34,23 @@ void Bryan::attaqueEnnemis(sf::RenderWindow* window, std::vector< sf::Sound >& a
 	case 1:
 		if (equipeEnnemi().estEnVie()) {
 			Affichage().dessinerTexte(nom() + " vise le plus faible",window);
-			DEGATS = degats(0.25, 0.50);
+			DEGATS = degats(0.15, 0.45);
 			indice = equipeEnnemi().plusFaible()->indiceEquipe();
-			RATIO = (equipeEnnemi()[indice]->vieMax() * 1.0) / (equipeEnnemi()[indice]->vie() * 1.0);
+			RATIO = (equipeEnnemi().perso(indice)->vieMax() * 1.0) / (equipeEnnemi().perso(indice)->vie() * 1.0);
 			RATIO = std::min(RATIO, 20.0);
 			DEGATS = static_cast<int>(DEGATS * RATIO);
-			Attaque(DEGATS, equipeEnnemi().plusFaible(),window,allSounds);
+			Attaque(DEGATS, equipeEnnemi().plusFaible(), C, window, allSounds);
 		}
 		ajouterMana(-1);
 		break;
 	case 2:
 		if (equipeEnnemi().estEnVie()) {
 			Affichage().dessinerTexte(nom() + " attaque par derrière ",window);
-			DEGATS = degats(1.0, 2.0);
-			AttaqueBrut(DEGATS, equipeEnnemi().plusLoinVivant(),window);
+			DEGATS = degats(0.75, 1.75);
+			AttaqueBrut(DEGATS, equipeEnnemi().plusLoinVivant(),C,window);
 			if (attaqueDouble() && equipeEnnemi().estEnVie()) {
-				DEGATS = degats(1.0, 2.0,CHOIXVITESSE);
-				AttaqueBrut(DEGATS, equipeEnnemi().plusFaible(),window);
+				DEGATS = degats(0.5, 1.5,CHOIXVITESSE);
+				AttaqueBrut(DEGATS, equipeEnnemi().plusFaible(),C,window);
 			}
 		}
 		ajouterMana(-2);
@@ -61,15 +61,14 @@ void Bryan::attaqueEnnemis(sf::RenderWindow* window, std::vector< sf::Sound >& a
 		ajouterDegatsCritique(5);
 		if (habile()) {
 			ajouterCoupCritique(1);
-			ajouterDegatsCritique(10);
-			ajouterReduction(5);
+			ajouterDegatsCritique(5);
 		}
 		SOINS = soins(0.5, 2.5);
 		if (vie() == vieMax()) {
-			bouclier(SOINS / 2, this,window);
+			bouclier(SOINS / 2, C, this, window);
 		}
 		else {
-			soigner(SOINS, this,window);
+			soigner(SOINS, C, this, window);
 		}
 		
 		ajouterMana(-3);
@@ -77,7 +76,7 @@ void Bryan::attaqueEnnemis(sf::RenderWindow* window, std::vector< sf::Sound >& a
 	}
 }
 
-void Bryan::passif(int tour, sf::RenderWindow* window, std::vector< sf::Sound >& allSounds)
+void Bryan::passif(int tour, Combat & C, sf::RenderWindow* window, std::vector< sf::Sound >& allSounds)
 {
 	if (status().estEmpoisoner()) {
 		status().soignerPoison();
@@ -85,17 +84,13 @@ void Bryan::passif(int tour, sf::RenderWindow* window, std::vector< sf::Sound >&
 	if (tour % 10 == 0) {
 		ajouterCoupCritique(1);
 		ajouterDegatsCritique(2);
-		if (habile()) {
-			
-			ajouterDegatsCritique(2);
-		}
 	}
 }
 
-void Bryan::passifDefensif(sf::RenderWindow* window, std::vector< sf::Sound >& allSounds, int degats, Personnage* P)
+void Bryan::passifDefensif(sf::RenderWindow* window, std::vector< sf::Sound >& allSounds, Combat & C, int degats, Personnage* P)
 {
 	if (Aleatoire(0, 101).entier() < pourcentageEsquive()) {
 		int SOINS = static_cast<int>(0.25 * vitesse());
-		bouclier(SOINS, this,window);
+		bouclier(SOINS, C, this, window);
 	}
 }
